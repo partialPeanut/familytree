e4e85dfe1c49835aaed6e406efa9439d15aa6766
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-02-12 23:09:13 -0700

    Add files via upload

--- sheetHandler.js
+++ sheetHandler.js
@@ -37,7 +37,6 @@ function initClient() {
         authorizeButton.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
     }, function(error) {
-        appendPre(JSON.stringify(error, null, 2));
     });
 }
 
