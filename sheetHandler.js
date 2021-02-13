1f48809038e461be92b723138f8c6e6809457042
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-02-13 01:06:29 -0700

    Add files via upload

--- sheetHandler.js
+++ sheetHandler.js
@@ -1,8 +1,8 @@
-var CLIENT_ID = '34121438684-a07qn39bisdcvvac89p690fdclhrtsc8.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyATtpy15MZCsqCjRWCpFjKWiOeSy7OTvq8';
+var CLIENT_ID = '34121438684-a07qn39bisdcvvac89p690fdclhrtsc8.apps.googleusercontent.com'
+var API_KEY = 'AIzaSyATtpy15MZCsqCjRWCpFjKWiOeSy7OTvq8'
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
@@ -18,7 +18,7 @@ console.log(signoutButton)
  */
 function handleClientLoad() {
     console.log("Running...")
-    gapi.load('client:auth2', initClient);
+    gapi.load('client:auth2', initClient)
 }
 
 /**
@@ -35,12 +35,12 @@ function initClient() {
     }).then(function () {
         console.log("Worked!")
         // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
 
         // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
+        authorizeButton.onclick = handleAuthClick
+        signoutButton.onclick = handleSignoutClick
     }, function(error) {
         console.log(error)
     });
@@ -52,12 +52,12 @@ function initClient() {
  */
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        listMajors();
+        authorizeButton.style.display = 'none'
+        signoutButton.style.display = 'block'
+        listNames()
     } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
+        authorizeButton.style.display = 'block'
+        signoutButton.style.display = 'none'
     }
 }
 
@@ -65,12 +65,32 @@ function updateSigninStatus(isSignedIn) {
  *  Sign in the user upon button click.
  */
 function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn()
 }
 
 /**
  *  Sign out the user upon button click.
  */
 function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
+    gapi.auth2.getAuthInstance().signOut()
+}
+
+function appendName(text) {
+    nameButton = document.querySelector('#namesList')
+    aName = document.createTextNode(text + '\n')
+    nameButton.appendChild(aName)
+}
+
+function listNames() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1tmPGcVRGJIzRfyHdBvNvPNYUoEfSKlbbklQR54dzoAQ',
+        range: 'Brothers!A1:A25'
+      }).then((response) => {
+        var result = response.result
+        for (i = 0; i < result.values.length; i++) {
+            appendName(range.values[i][0])
+        }
+        var numRows = result.values ? result.values.length : 0
+        console.log(`${numRows} rows retrieved.`)
+      })
 }
