4b2d2733918327cc2fae84936e55f6b25b6df1b0
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-02-13 15:21:06 -0700

    Add files via upload

--- treeFxns.js
+++ treeFxns.js
@@ -1,8 +1,13 @@
-function setHeightRecursively(siblingName, fullJSON, height) {
-    sibling = fullJSON[siblingName]
-    sibling.height = height
+// Converts a single sibling into a JSON format
+function rowToJSON(row) {
+    sibJSON = {}
+    sibJSON.name = row[0]
+    sibJSON.pledgeClass = row[1]
+    sibJSON.gradYear = row[2]
+    sibJSON.bigName = row[3] == 'XXX' ?  null : row[3]
+    sibJSON.littleNames = []
+    sibJSON.house = row[4] == 'XXX' ? null : row[4]
+    sibJSON.tags = row[5] == 'XXX' ? [] : row[5].split(';')
 
-    sibling.littleNames.forEach(littleName => {
-        setHeightRecursively(littleName, fullJSON, height+1)
-    })
+    return sibJSON
 }
