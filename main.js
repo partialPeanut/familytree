f85a3e963dd507876481d0e77e70762cdc51c807
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-02-13 15:08:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,82 @@
+// Get raw spreadsheet data and convert it into a dope-ass data structure.
+function placeSiblings(result) {
+    console.log("Place Siblings: Workable Ver.")
+
+    // Log the number of siblings
+    var numRows = result.values ? result.values.length : 0
+    console.log(`${numRows} siblings retrieved.`)
+
+    // Put all siblings into a JSON structure. This only works if the sheet is properly sorted and there are no spelling errors.
+    siblings = {'0': {}}
+    for (x = 0; x < result.values.length; x++) {
+        sibJSON = rowToJSON(result.values[x])
+        // If the sib has no big, they're at height 0.
+        if (sibJSON.bigName === null) {
+            // If they have no house, they're in the FoLS, otherwise, they're a founder
+            if (sibJSON.house === null) sibJSON.house = "Field of Lost Souls"
+            else sibJSON.tags.push("Founder")
+
+            // Place them in the structure at height 0
+            sibJSON.height = 0
+            siblings['0'][sibJSON.name] = sibJSON
+        }
+        else {
+            found = false
+
+            // Scan iteratively through each structure level for the sib's big
+            $.each(siblings, function(key, val) {
+                i = parseInt(key)
+                //console.log("key is: " + key + " and i is: " + i)
+                if (val.hasOwnProperty(sibJSON.bigName)) {
+                    //console.log(sibJSON.name + "'s big is in this layer!")
+                    found = true
+
+                    // Create a new level if necessary
+                    if (!siblings.hasOwnProperty(i+1)) {
+                        //console.log("Creating new layer: " + (i+1))
+                        siblings[i+1] = {}
+                        //console.log("Siblings is now: " + JSON.stringify(siblings))
+                    }
+
+                    // If they have no house, inherit their big's, otherwise, they're a founder
+                    if (sibJSON.house === null) sibJSON.house = val[sibJSON.bigName].house
+                    else sibJSON.tags.push("Founder")
+
+                    // Add themselves to their big's list of littles
+                    val[sibJSON.bigName].littleNames.push(sibJSON.name)
+
+                    // Place them in the structure at the level just below their big
+                    sibJSON.height = i+1
+                    siblings[i+1][sibJSON.name] = sibJSON
+                    return false
+                } //else console.log(sibJSON.name + "'s big is not in this layer.")
+            })
+
+            // There has been some error or misspelling.
+            if (!found) {
+                // Create a new level if necessary
+                if (!siblings.hasOwnProperty('-1')) {
+                    console.log("Creating error layer.")
+                    siblings['-1'] = {}
+                }
+
+                // If they have no house, inherit their big's, otherwise, they're a founder
+                if (sibJSON.house === null) sibJSON.house = "Field of Lost Souls"
+                else sibJSON.tags.push("Founder")
+
+                // Place them in the structure at height -1
+                sibJSON.height = -1
+                siblings['-1'][sibJSON.name] = sibJSON
+            }
+        }
+    }
+
+    console.log("Siblings:")
+    console.log(siblings)
+
+    drawTree()
+}
+
 function drawTree() {
     console.log("Main: Slap Em Down ver. 1")
 
--- sheetHandler.js
+++ sheetHandler.js
@@ -72,20 +72,6 @@ function handleSignoutClick(event) {
     gapi.auth2.getAuthInstance().signOut()
 }
 
-// Converts a single sibling into a JSON format
-function rowToJSON(row) {
-    sibJSON = {}
-    sibJSON.name = row[0]
-    sibJSON.pledgeClass = row[1]
-    sibJSON.gradYear = row[2]
-    sibJSON.bigName = row[3] == 'XXX' ?  null : row[3]
-    sibJSON.littleNames = []
-    sibJSON.house = row[4] == 'XXX' ? null : row[4]
-    sibJSON.tags = row[5] == 'XXX' ? [] : row[5].split(';')
-
-    return sibJSON
-}
-
 // Get raw spreadsheet data and convert it into a dope-ass data structure.
 function placeSiblings() {
     console.log("Place Siblings: Workable Ver. Final?")
@@ -95,80 +81,6 @@ function placeSiblings() {
         spreadsheetId: '1tmPGcVRGJIzRfyHdBvNvPNYUoEfSKlbbklQR54dzoAQ',
         range: 'Brothers!A2:F'
       }).then((response) => {
-        var result = response.result
-        
-        // Log the number of siblings
-        var numRows = result.values ? result.values.length : 0
-        console.log(`${numRows} siblings retrieved.`)
-
-        // Put all siblings into a JSON structure. This only works if the sheet is properly sorted and there are no spelling errors.
-        siblings = {'0': {}}
-        for (x = 0; x < result.values.length; x++) {
-            sibJSON = rowToJSON(result.values[x])
-            // If the sib has no big, they're at height 0.
-            if (sibJSON.bigName === null) {
-                // If they have no house, they're in the FoLS, otherwise, they're a founder
-                if (sibJSON.house === null) sibJSON.house = "Field of Lost Souls"
-                else sibJSON.tags.push("Founder")
-
-                // Place them in the structure at height 0
-                sibJSON.height = 0
-                siblings['0'][sibJSON.name] = sibJSON
-            }
-            else {
-                found = false
-
-                // Scan iteratively through each structure level for the sib's big
-                $.each(siblings, function(key, val) {
-                    i = parseInt(key)
-                    //console.log("key is: " + key + " and i is: " + i)
-                    if (val.hasOwnProperty(sibJSON.bigName)) {
-                        //console.log(sibJSON.name + "'s big is in this layer!")
-                        found = true
-
-                        // Create a new level if necessary
-                        if (!siblings.hasOwnProperty(i+1)) {
-                            //console.log("Creating new layer: " + (i+1))
-                            siblings[i+1] = {}
-                            //console.log("Siblings is now: " + JSON.stringify(siblings))
-                        }
-
-                        // If they have no house, inherit their big's, otherwise, they're a founder
-                        if (sibJSON.house === null) sibJSON.house = val[sibJSON.bigName].house
-                        else sibJSON.tags.push("Founder")
-
-                        // Add themselves to their big's list of littles
-                        val[sibJSON.bigName].littleNames.push(sibJSON.name)
-
-                        // Place them in the structure at the level just below their big
-                        sibJSON.height = i+1
-                        siblings[i+1][sibJSON.name] = sibJSON
-                        return false
-                    } //else console.log(sibJSON.name + "'s big is not in this layer.")
-                })
-
-                // There has been some error or misspelling.
-                if (!found) {
-                    // Create a new level if necessary
-                    if (!siblings.hasOwnProperty('-1')) {
-                        console.log("Creating error layer.")
-                        siblings['-1'] = {}
-                    }
-
-                    // If they have no house, inherit their big's, otherwise, they're a founder
-                    if (sibJSON.house === null) sibJSON.house = "Field of Lost Souls"
-                    else sibJSON.tags.push("Founder")
-
-                    // Place them in the structure at height -1
-                    sibJSON.height = -1
-                    siblings['-1'][sibJSON.name] = sibJSON
-                }
-            }
-        }
-
-        console.log("Siblings:")
-        console.log(siblings)
-
-        drawTree()
+        placeSiblings(response.result)
       })
 }
