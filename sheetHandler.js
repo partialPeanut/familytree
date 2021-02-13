7635d09ad2261bcf3c0c7ba0e970baac4c295c6d
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-02-13 14:17:26 -0700

    Add files via upload

--- sheetHandler.js
+++ sheetHandler.js
@@ -87,7 +87,7 @@ function rowToJSON(row) {
 }
 
 function placeSiblings() {
-    console.log("Place siblings: Layer Spam ver. 4")
+    console.log("Place siblings: Layer Spam ver. 5")
 
     // Get the spreadsheet
     gapi.client.sheets.spreadsheets.values.get({
@@ -118,15 +118,15 @@ function placeSiblings() {
                 // Scan iteratively through each structure level for the sib's big
                 $.each(siblings, function(key, val) {
                     i = parseInt(key)
-                    console.log("key is: " + key + " and i is: " + i)
+                    //console.log("key is: " + key + " and i is: " + i)
                     if (val.hasOwnProperty(sibJSON.bigName)) {
-                        console.log(sibJSON.name + "'s big is in this layer!")
+                        //console.log(sibJSON.name + "'s big is in this layer!")
 
                         // Create a new level if necessary
                         if (!siblings.hasOwnProperty(i+1)) {
-                            console.log("Creating new layer.")
+                            console.log("Creating new layer: " + (i+1))
                             siblings[i+1] = {}
-                            console.log("Siblings is now: " + JSON.stringify(siblings))
+                            //console.log("Siblings is now: " + JSON.stringify(siblings))
                         }
 
                         // If they have no house, inherit their big's, otherwise, they're a founder
@@ -136,7 +136,7 @@ function placeSiblings() {
                         // Place them in the structure at the level just below their big
                         sibJSON.height = i+1
                         siblings[i+1][sibJSON.name] = sibJSON
-                    } else console.log(sibJSON.name + "'s big is not in this layer.")
+                    } //else console.log(sibJSON.name + "'s big is not in this layer.")
                 })
             }
         }
