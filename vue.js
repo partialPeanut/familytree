4d0d11884182d8208542cf08f53e1b262bc3e650
partialPeanut <76558249+partialPeanut@users.noreply.github.com>
2021-03-05 21:25:34 -0700

    Add files via upload

--- vue.js
+++ vue.js
@@ -9,19 +9,15 @@ function loadVue() {
     appElement = new Vue({
         el: '#app',
         data: {
-            displayTab: 'tree'
-        }
-    })
-
-    nameTab = new Vue({
-        el: '#nameTab',
-        data: {
-            name: 'Name',
-            pledgeClass: 'Pledge Class',
-            house: 'House',
-            tags: ['Tag 1', 'Tag 2'],
-            bigName: 'Big Name',
-            littleNames: ['Little 1', 'Little 2']
+            displayTab: 'tree',
+            nameTabData: {
+                name: 'Name',
+                pledgeClass: 'Pledge Class',
+                house: 'House',
+                tags: ['Tag 1', 'Tag 2'],
+                bigName: 'Big Name',
+                littleNames: ['Little 1', 'Little 2']
+            }
         }
     })
 }
