56e0e9f073d8f864504158d952895dc75cbcfd43
partialPeanut <penny@scharftech.com>
2021-07-26 16:16:23 -0600

    Praise jank

--- main.js
+++ main.js
@@ -169,9 +169,15 @@ function createTabContainerDivs() {
     })
     tabs = $( tcc ).tabs({
         create: function(e, ui) {
+            panels = tabs.find( ".ui-tabs-panel" )
+            panels.removeAttr('aria-hidden')
+            panels.css("display", "")
             ui.panel.height('100%')
         },
         activate: function(e, ui) {
+            panels = tabs.find( ".ui-tabs-panel" )
+            panels.removeAttr('aria-hidden')
+            panels.css("display", "")
             ui.oldPanel.height('0%')
             ui.newPanel.height('100%')
         }
